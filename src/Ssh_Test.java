b5989ad937c952fedaa58faf5ac0830b7217bb77
wjhwang <wjhwang@samsung.com>
2016-07-01 10:23:16 +0900

    ssh-test.java 생성

---
+++ src/Ssh_Test.java
@@ -0,0 +1,15 @@
+import java.nio.channels.Channel;
+
+public class Ssh_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Ssh_Test ssh = new Ssh_Test();
+		ssh.run();
+	}
+
+	private void run(){
+
+	}
+}
